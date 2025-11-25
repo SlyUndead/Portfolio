9db0109dc5ec5322d980e805c7f3012ca4df31e3
Prem Parikh <premparikh4@gmail.com>
2025-11-24 23:23:27 -0500

    no cors

--- tracker.js
+++ tracker.js
@@ -110,6 +110,7 @@
             try {
                 await fetch(WEBHOOK_URL, {
                     method: 'POST',
+                    mode: 'no-cors',
                     headers: {
                         'Content-Type': 'application/json',
                     },
